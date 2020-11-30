77ed841f045ebc3d95744e1927d58c53d5680fb5
Anton Stöckl <anton.stoeckl@maibornwolff.de>
2020-11-30 11:06:19 +0100

    Prepare code for challenge

--- src/main/java/domain/functional/es/customer/Customer7.java
+++ src/main/java/domain/functional/es/customer/Customer7.java
@@ -9,31 +9,14 @@ import java.util.List;
 
 public class Customer7 {
     public static CustomerRegistered register(RegisterCustomer command) {
-        return CustomerRegistered.build(
-                command.customerID,
-                command.emailAddress,
-                command.confirmationHash,
-                command.name
-        );
+        return null; // TODO
     }
 
     public static List<Event> confirmEmailAddress(CustomerState current, ConfirmCustomerEmailAddress command) {
-        if (!current.confirmationHash.equals(command.confirmationHash)) {
-            return List.of(CustomerEmailAddressConfirmationFailed.build(command.customerID));
-        }
-
-        if (current.isEmailAddressConfirmed) {
-            return List.of();
-        }
-
-        return List.of(CustomerEmailAddressConfirmed.build(command.customerID));
+        return List.of(); // TODO
     }
 
     public static List<Event> changeEmailAddress(CustomerState current, ChangeCustomerEmailAddress command) {
-        if (current.emailAddress.equals(command.emailAddress)) {
-            return List.of();
-        }
-
-        return List.of(CustomerEmailAddressChanged.build(command.customerID, command.emailAddress, command.confirmationHash));
+        return List.of(); // TODO
     }
 }
